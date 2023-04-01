1dae5617d19b3e3a8d75b66768d809c14569f87f
tdas11235 <tdas11235@gmail.com>
2023-04-01 14:20:42 +0530

    mails extracted

--- src/index.js
+++ src/index.js
@@ -11,41 +11,41 @@ const TOKEN_PATH = path.join(__dirname, "token.json");
 const CREDENTIALS_PATH = path.join(__dirname, "credentials.json");
 
 async function loadSavedCredentialsIfExist() {
-    try {
-        const content = await fs.readFile(TOKEN_PATH);
-        const credentials = JSON.parse(content);
-        return google.auth.fromJSON(credentials);
-    } catch (err) {
-        return null;
-    }
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
 }
 
 async function saveCredentials(client) {
-    const content = await fs.readFile(CREDENTIALS_PATH);
-    const keys = JSON.parse(content);
-    const key = keys.installed || keys.web;
-    const payload = JSON.stringify({
-        type: "authorized_user",
-        client_id: key.client_id,
-        client_secret: key.client_secret,
-        refresh_token: client.credentials.refresh_token,
-    });
-    await fs.writeFile(TOKEN_PATH, payload);
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: "authorized_user",
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
 }
 
 async function authorize() {
-    let client = await loadSavedCredentialsIfExist();
-    if (client) {
-        return client;
-    }
-    client = await authenticate({
-        scopes: SCOPES,
-        keyfilePath: CREDENTIALS_PATH,
-    });
-    if (client.credentials) {
-        await saveCredentials(client);
-    }
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
     return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
 }
 
 // async function listLabels(auth) {
@@ -65,51 +65,57 @@ async function authorize() {
 // }
 
 const gamilData = async (auth) => {
-    const gmail = google.gmail({ version: "v1", auth });
-    let response;
-    let messages;
-    try {
-        response = await gmail.users.messages.list({
-            userId: 'me',
-            labelIds: ['INBOX'],
-            maxResults: 10,
-        });
-        console.log(response);
-        const promises = response.result.messages.map(async (mail) => {
-            let data = await gmail.users.messages.get({
-                userId: 'me',
-                id: mail.id,
-            });
-            return data.result.snippet;
-        })
-        messages = await Promise.all(promises);
-        console.log(messages);
-        return messages;
+  const gmail = google.gmail({ version: "v1", auth });
+  let response;
+  let messages;
+  try {
+    response = await gmail.users.messages.list({
+      userId: "me",
+      labelIds: ["INBOX"],
+      maxResults: 50,
+    });
+    console.log(response.data.messages[0].id);
+    const promises = response.data.messages.map(async (mail) => {
+      let _data = await gmail.users.messages.get({
+        userId: "me",
+        id: mail.id,
+      });
+      //   console.log(_data);
+      return _data.data.snippet;
+    });
+    messages = await Promise.all(promises);
+    console.log(messages);
+    return messages;
+  } catch (err) {
+    // document.getElementById('content').innerText = err.message;
+    // console.log(err);
+    return null;
+  }
+  // const labels = messages;
+  // if (!labels || labels.length == 0) {
+  //     document.getElementById('content').innerText = 'No labels found.';
+  //     return;
+  // }
+  // Flatten to string to display
+  // const output = labels.reduce(
+  //     (str, label) => `${str}${label}\n`,
+  //     'Labels:\n');
+  // document.getElementById('content').innerText = labels;
+};
 
-    } catch (err) {
-        // document.getElementById('content').innerText = err.message;
-        return null;
-    }
-    // const labels = messages;
-    // if (!labels || labels.length == 0) {
-    //     document.getElementById('content').innerText = 'No labels found.';
-    //     return;
-    // }
-    // Flatten to string to display
-    // const output = labels.reduce(
-    //     (str, label) => `${str}${label}\n`,
-    //     'Labels:\n');
-    // document.getElementById('content').innerText = labels;
-}
-
-app.get("/mails", (req, resp) => {
-    let data = [];
-    authorize().then((res) => {
-        data = gamilData(res);
-    })
-    resp.send(data);
-})
+app.post("/mails", async (req, res) => {
+  let data = [];
+  try {
+    let result = await authorize();
+    data = await gamilData(result);
+    console.log(data.length);
+    res.send(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
 
 app.listen(8000, () => {
-    console.log("Server Started")
-})
+  console.log("Server Started");
+});
